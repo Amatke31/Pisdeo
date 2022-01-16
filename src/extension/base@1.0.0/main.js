9b39ce16ae7edc0f0c2ca112d00d39fa9f79e53d
Amatke31 <2352595327@qq.com>
2022-01-16 23:50:24 +0800

    删除原来的扩展代码

--- src/extension/base@1.0.0/main.js
+++ src/extension/base@1.0.0/main.js
@@ -1,24 +1,3 @@
-// const { template, Extension } = require('nexwebeditor-extension')
-
-// class StaticExtension extends Extension {
-//     onInit() {
-//         template.add({
-//             name: 'My HomePage',
-//             id: "org.nexwebeditor.template.myhomepage"
-//         })
-//     }
-
-//     useTemplate(project, templateInfo, complete) {
-//         console.log(project)
-//         template.copyTemplate('./template', project)
-//         complete({
-//             complete: "success"
-//         })
-//     }
-// }
-
-// module.exports = StaticExtension
-
 module.exports = (api) => {
     api.addTemplate({
         name: 'My HomePage',
