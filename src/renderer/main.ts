e0c38e07614f033846a35b87d1b2c050b5ed0ee3
Amatke31 <2352595327@qq.com>
2022-02-03 14:35:39 +0800

    增加elementui

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -6,6 +6,7 @@ import fs from 'fs'
 import path from 'path'
 import axios from 'axios'
 import platform from './utils/platform/platform'
+import installElementPlus from './plugins/element'
 
 const app = createApp(App)
 if (platform === "desktop") {
@@ -14,4 +15,5 @@ if (platform === "desktop") {
 app.config.globalProperties.$fs = fs
 app.config.globalProperties.$path = path
 app.config.globalProperties.$axios = axios
-app.use(i18n).component('msg', Msg).mount('#_nexwebeditor')
+installElementPlus(app)
+app.use(i18n).component('msg', Msg).mount('#_nexwebdesigner')
