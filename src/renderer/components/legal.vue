0958ab51fa33de5a797958699a46155f0354b76b
Amatke31 <2352595327@qq.com>
2022-02-05 17:04:23 +0800

    legal测试文件

---
+++ src/renderer/components/legal.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="legal" v-html="legal" style="overflow-y: scroll;" />
+</template>
+<script>
+import ipc from "../utils/platform/desktop/ipc";
+import platform from "../utils/platform/platform";
+import MarkdownIt from "markdown-it";
+export default {
+    data() {
+        return {
+            legal: "loading...",
+        };
+    },
+    mounted: async function () {
+        let md = new MarkdownIt();
+        if (platform === "desktop") {
+            this.legal = md.render(await ipc.getLegal());
+        }
+    },
+};
+</script>
+<style lang="scss">
+#legal {
+    h1 {
+        font-size: 22px;
+    }
+    h2 {
+        font-size: 20px;
+    }
+    h3 {
+        font-size: 18px;
+    }
+    h4 {
+        font-size: 16px;
+    }
+    h5 {
+        font-size: 14px;
+    }
+    h6 {
+        font-size: 12px;
+    }
+    p {
+        font-size: 15px;
+    }
+}
+#legal {
+    color: rgba(0, 0, 0, 0);
+    transition: color 0.3s ease;
+}
+#legal:hover {
+    color: rgba(0, 0, 0, 0.3);
+}
+</style>
---
+++ static/legal/legal_en.md
@@ -0,0 +1,13 @@
+# NexWebDesigner Terms of Use
+
+## h2
+
+### h3
+
+#### h4
+
+##### h5
+
+###### h6
+
+p
