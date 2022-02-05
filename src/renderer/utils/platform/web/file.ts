08b785f0c0d7f0d0ec8423ce36d76ab54d823287
Amatke31 <2352595327@qq.com>
2022-02-05 19:03:38 +0800

    初步写indexddb

---
+++ src/renderer/utils/platform/web/file.ts
@@ -0,0 +1,11 @@
+function openDB(name: string, db: any) {
+    var request = window.indexedDB.open(name);
+    request.onerror = function (e) {
+        console.log('Open Error!');
+    };
+    request.onsuccess = function (e: any) {
+        db.db = e.target.result;
+    };
+}
+
+export { openDB }
