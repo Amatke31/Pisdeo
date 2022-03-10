0a4888c4bf579e66edd698bfc4df709bbfa219d5
Amatke31 <2352595327@qq.com>
2022-03-10 13:28:55 +0800

    更换到新版element-plus

--- src/renderer/plugins/element.ts
+++ src/renderer/plugins/element.ts
@@ -1,6 +1,8 @@
 import type { App } from "vue";
 import ElementPlus from 'element-plus'
-import 'element-theme-dark';
+import 'element-plus/dist/index.css'
+// import 'element-theme-dark';
+import 'element-plus-dark/index.scss'
 
 export default (app: App<Element>): void => {
     app.use(ElementPlus)
