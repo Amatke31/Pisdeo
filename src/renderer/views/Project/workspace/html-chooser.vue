7c5818a33f431f56526dfc9f30ea0da0c5e003e6
Amatke31 <2352595327@qq.com>
2022-03-11 10:23:30 +0800

    改样式

--- src/renderer/views/Project/workspace/html-chooser.vue
+++ src/renderer/views/Project/workspace/html-chooser.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="html-chooser">
-
-    </div>
+    <div class="html-chooser"></div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    mounted: function() {},
+    data(){
+        return {
+            html: {}
+        }
+    },
+    mounted: function() {
+    },
 });
 </script>
 <style lang="scss" scoped>
 .html-chooser {
+    margin: 4px;
     width: 20vw;
     height: 60vh;
     background-color: #222;
     border-radius: 8px;
-    border: 1px #888;
+    border: 1px solid #555;
 }
 </style>
