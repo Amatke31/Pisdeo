177919e46d3b957ccc69d8b61e00635bdecc4811
Amatke31 <2352595327@qq.com>
2022-03-25 13:17:57 +0800

    升级vuetify

--- src/main/plugins/vuetify.ts
+++ src/main/plugins/vuetify.ts
@@ -1,10 +1,13 @@
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/lib/styles/main.sass'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components/index'
-import * as directives from 'vuetify/lib/directives/index'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/lib/styles/main.sass";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/lib/components/index";
+import * as directives from "vuetify/lib/directives/index";
 
 export default createVuetify({
     components,
-    directives
-})
+    directives,
+    theme: {
+        defaultTheme: "dark",
+    },
+});
