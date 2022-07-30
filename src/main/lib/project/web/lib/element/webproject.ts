73a8ced5e566741e959169cfe84a81a681d508a0
Amatke31 <2352595327@qq.com>
2022-07-30 13:49:58 +0800

    修复问题

--- src/main/lib/project/web/lib/element/webproject.ts
+++ src/main/lib/project/web/lib/element/webproject.ts
@@ -1,4 +1,5 @@
 import { ObjToCSS, clearCss } from "../css/selector";
+import { arrRemove } from "@/main/utils/array";
 const noChange = ["element", "children", "css", "script", "text", "elementName"];
 const singleElemenet = ["img", "input", "br"];
 
@@ -38,4 +39,136 @@ function analysisObj(obj: any): HTMLElement | string {
     }
 }
 
-export { ObjToElement };
+class ElementControl {
+    currentOpenFile = null;
+    htmlChoose = "layer-0";
+    refreshChooser = () => {};
+    refreshViewer = () => {};
+    refreshAttr = () => {};
+    constructor(that: any) {
+        this.currentOpenFile = that.currentOpenFile;
+        this.htmlChoose = that.htmlChoose;
+        this.refreshChooser = that.refreshChooser;
+        this.refreshViewer = that.refreshViewer;
+        this.refreshAttr = that.refreshAttr;
+    }
+
+    addElement(element: string): void {
+        this._addElement(this.currentOpenFile, this.htmlChoose.split("-"), 2, element);
+    }
+    private _addElement(obj: any, path: any, layer: number, element: any) {
+        if (path.length == 2) {
+        } else if (path.length == layer) {
+            let next = obj;
+            if (!next.children) {
+                next.children = [];
+            }
+            next.children.push(element);
+            this.refreshChooser();
+            setTimeout(() => {
+                this.htmlChoose = `${this.htmlChoose}-${next.children.length.toString() - 1}`;
+            }, 1);
+            this.refreshViewer();
+        } else {
+            this._addElement(obj.children[Number(path[layer])], path, layer + 1, element);
+        }
+    }
+
+    removeElement(): void {
+        this._removeElement(this.currentOpenFile, this.htmlChoose.split("-"), 2);
+    }
+    private _removeElement(obj: any, path: any, layer: number) {
+        if (path.length == 2) {
+        } else if (path.length == layer + 1) {
+            let next = obj;
+            const last = path.pop();
+            this.htmlChoose = this.htmlChoose.slice(0, this.htmlChoose.length - last.length - 1);
+            next.children = arrRemove(last, next.children);
+            this.refreshViewer();
+            setTimeout(() => this.refreshChooser(), 1);
+        } else {
+            this._removeElement(obj.children[Number(path[layer])], path, layer + 1);
+        }
+    }
+
+    get getAttribute() {
+        const out = this._getAttribute(this.currentOpenFile, this.htmlChoose.split("-"), 2);
+        return out;
+    }
+    private _getAttribute(obj: any, path: any, layer: number) {
+        if (path.length == 2) {
+            return obj;
+        } else if (path.length == layer) {
+            return obj;
+        } else {
+            return this._getAttribute(obj.children[Number(path[layer])], path, layer + 1);
+        }
+    }
+
+    setAttribute(e: any, noRefresh: boolean = false) {
+        this._setAttribute(this.currentOpenFile, this.htmlChoose.split("-"), 2, e, noRefresh);
+    }
+    private _setAttribute(obj: any, path: any, layer: number, changeAttr: any, noRefresh: boolean) {
+        if (path.length == 2) {
+        } else if (path.length == layer) {
+            let next = obj;
+            next[changeAttr.changeAttr] = changeAttr.value;
+            if (changeAttr.changeAttr == "elementName" || changeAttr.changeAttr == "element") {
+                this.refreshChooser();
+            }
+            this.refreshAttr();
+            if (!noRefresh) {
+                this.refreshViewer();
+            }
+        } else {
+            this._setAttribute(
+                obj.children[Number(path[layer])],
+                path,
+                layer + 1,
+                changeAttr,
+                noRefresh
+            );
+        }
+    }
+
+    delAttribute(e: any, noRefresh: boolean = false) {
+        this._delAttribute(this.currentOpenFile, this.htmlChoose.split("-"), 2, e, noRefresh);
+    }
+    private _delAttribute(obj: any, path: any, layer: number, changeAttr: any, noRefresh: boolean) {
+        if (path.length == 2) {
+        } else if (path.length == layer) {
+            let next = obj;
+            delete next[changeAttr.changeAttr];
+            this.refreshAttr();
+            if (!noRefresh) {
+                this.refreshViewer();
+            }
+        } else {
+            this._delAttribute(
+                obj.children[Number(path[layer])],
+                path,
+                layer + 1,
+                changeAttr,
+                noRefresh
+            );
+        }
+    }
+
+    setAttributeT(e: any) {
+        this._setAttributeT(this.currentOpenFile, this.htmlChoose.split("-"), 2, e);
+    }
+    private _setAttributeT(obj: any, path: any, layer: number, changeAttr: any) {
+        if (path.length == 2) {
+        } else if (path.length == layer) {
+            let next = obj;
+            next[changeAttr.value] = next[changeAttr.changeAttr];
+            delete next[changeAttr.changeAttr];
+            this.refreshAttr();
+            this.refreshViewer();
+        } else {
+            this._setAttributeT(obj.children[Number(path[layer])], path, layer + 1, changeAttr);
+        }
+    }
+}
+
+export { ObjToElement, ElementControl };
